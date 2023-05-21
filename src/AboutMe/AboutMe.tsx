e478b259da1153fae694333a0ac45d1733881ad1
Angster770 <angster770@gmail.com>
2023-05-21 14:26:22 -0400

    added fade in for about

--- src/AboutMe/AboutMe.tsx
+++ src/AboutMe/AboutMe.tsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Typography, Paper, Grid } from "@mui/material"
 import styled from "@emotion/styled"
+import { animateIntroSection } from "../LandingPage/MainPage"
 
 const AboutMeContainer = styled(Grid)`
   background-color: #fcfcf9;
@@ -10,9 +11,11 @@ const AboutMeContainer = styled(Grid)`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
-  /* Large screens */
-  @media (min-width: 56.25rem) {
+  height: 100vh;
+  padding-top: 10rem;
+  // margin-bottom: 8rem;
+  padding-bottom: 10rem;
+  font-size: 10rem /* Large screens */ @media (min-width: 56.25rem) {
     /* Styles for screens 900px and above */
     flex-basis: 100%;
     max-width: 100%;
@@ -21,16 +24,16 @@ const AboutMeContainer = styled(Grid)`
 `
 
 export const AboutMe: () => JSX.Element = () => {
+  useEffect(() => {
+    animateIntroSection(".about-me")
+  }, [])
   return (
     <>
-      <AboutMeContainer>
-        <Typography style={{ fontFamily: "Helvetica Neue, sans-serif" }} variant="h4" gutterBottom>
+      <AboutMeContainer className="about-me">
+        <Typography style={{ fontFamily: "Helvetica Neue, sans-serif" }} variant="h2" gutterBottom>
           About Me
         </Typography>
-        <Typography
-          style={{ fontFamily: "Helvetica Neue, sans-serif", padding: "1rem", maxWidth: "50%" }}
-          variant="body1"
-        >
+        <Typography style={{ fontFamily: "Helvetica Neue, sans-serif", padding: "1rem", maxWidth: "50%" }} variant="h4">
           I was born 1996 in Baltimore, Maryland. When not coding I enjoy other things like cooking, reading and
           practicing violin.
         </Typography>
